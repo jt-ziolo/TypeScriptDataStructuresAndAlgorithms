2b6be0beb890ee229ed5081e7d4dab760ae43442
JT Ziolo <ziolojt@gmail.com>
2024-04-09 14:22:34 -0600

    feat: add skip list functionality, tests wip

--- src/data-structures/skip-list.test.ts
+++ src/data-structures/skip-list.test.ts
@@ -3,6 +3,10 @@ import { SkipList } from "./skip-list";
 import { arrayCopyToFunction, arrayEmptyConstructor } from "../util";
 
 const probabilityFunctions = {
+  always() {
+    // will cause an infinite loop if maxPromotions is not set
+    return true;
+  },
   half() {
     return randomInt(2) === 0;
   },
@@ -32,6 +36,23 @@ describe("skip list", () => {
         expect(list.length).toBe(0);
       });
     });
+    describe("empty array collection parameter passed in", () => {
+      let list: SkipList<number>;
+      beforeEach(() => {
+        list = new SkipList<number>(
+          probabilityFunctions.half,
+          createCollectionParams([]),
+        );
+      });
+
+      it("does not assign a head", () => {
+        expect(list.head).toBeUndefined();
+      });
+
+      it("returns zero length", () => {
+        expect(list.length).toBe(0);
+      });
+    });
     describe("includes collection params", () => {
       let list: SkipList<number>;
       const inputArray = [1, 2, 3, 4, 5];
@@ -50,15 +71,29 @@ describe("skip list", () => {
         expect(list.length).toBe(5);
       });
 
-      it("returns the correct values when traversed", () => {
-        for (
-          let node = list.head, i = 0;
-          node !== undefined;
-          node = node.next, i++
-        ) {
-          expect(node.data).toBe(inputArray.at(i));
+      it("returns the correct values for the lowest layer", () => {
+        expect(list.values).toStrictEqual([1, 2, 3, 4, 5]);
+      });
+    });
+    describe("includes maxPromotions arg", () => {
+      let list: SkipList<number>;
+      const inputArray = [1, 2, 3, 4, 5];
+      beforeEach(() => {
+        list = new SkipList<number>(
+          probabilityFunctions.always,
+          createCollectionParams(inputArray),
+          10,
+        );
+      });
+
+      it("results in a list 10 layers deep", () => {
+        let count = 0;
+        for (let node = list.head!; node.down !== undefined; node = node.down) {
+          count += 1;
         }
+        expect(count).toBe(10);
       });
     });
   });
+  describe.skip("insert", () => {});
 });
--- src/data-structures/skip-list.ts
+++ src/data-structures/skip-list.ts
@@ -10,6 +10,7 @@ import {
   CollectionConstructor,
   CollectionCopyToFunction,
   HasLength,
+  deleteObjectProperties,
   isSorted,
 } from "../util";
 import { LinkedNode } from "./linked-list";
@@ -29,7 +30,8 @@ export class SkipListNode<T> implements LinkedNode<T> {
 
 export class SkipList<T> implements HasLength {
   head?: SkipListNode<T>;
-  #probabilityFunction: ProbabilityFunction;
+  readonly #probabilityFunction: ProbabilityFunction;
+  readonly #maxPromotions: number;
 
   constructor(
     probabilityFunction: ProbabilityFunction,
@@ -38,8 +40,10 @@ export class SkipList<T> implements HasLength {
       copyToFunction: CollectionCopyToFunction<T>;
       constructor: CollectionConstructor<T>;
     },
+    maxPromotions: number = Infinity,
   ) {
     this.#probabilityFunction = probabilityFunction;
+    this.#maxPromotions = maxPromotions;
     const collection = collectionParams?.collection;
     if (collection === undefined || collection.length === 0) {
       return;
@@ -54,22 +58,23 @@ export class SkipList<T> implements HasLength {
       );
     }
 
-    this.head = new SkipListNode<T>(collection.at(0)!);
-    for (
-      let node = this.head, i = 1, length = collection.length;
-      i < length;
-      i++
-    ) {
-      node.next = new SkipListNode<T>(collection.at(i)!);
-      node.nextWidth = 1;
-      node = node.next;
+    for (const item of collection) {
+      this.insert(item);
     }
   }
 
   get length(): number {
-    let count = 0;
-    for (let node = this.head; node !== undefined; node = node.next) {
-      count++;
+    if (this.head === undefined) {
+      return 0;
+    }
+    let count = 1;
+    let node: SkipListNode<T> = this.head;
+    while (node.down !== undefined) {
+      node = node.down;
+    }
+    while (node.next !== undefined) {
+      node = node.next;
+      count += 1;
     }
     return count;
   }
@@ -79,10 +84,166 @@ export class SkipList<T> implements HasLength {
   }
 
   insert(value: T) {
-    throw new Error("Not yet implemented.");
+    if (this.head === undefined) {
+      this.head = new SkipListNode<T>(value);
+      return;
+    }
+
+    // traverse the list, tracking the nodes that we descend from as we go
+    let node = this.head;
+    let previousNode = undefined;
+    const descendedNodesByLayer: Array<SkipListNode<T>> = [];
+    while (true) {
+      if (node.data === value) {
+        // do not insert duplicate nodes into a skip list
+        return;
+      }
+      if (node.data < value) {
+        previousNode = node;
+        if (node.next !== undefined) {
+          // check next node in current layer
+          node = node.next;
+        } else if (node.down !== undefined) {
+          // descend
+          descendedNodesByLayer.push(node);
+          node = node.down;
+          previousNode = undefined;
+        } else {
+          // we are at the end of the list on the bottom layer
+          // insert after the current node
+          const insertedNode = this.#insertAfter(value, node);
+          this.#setupPromotions(insertedNode, descendedNodesByLayer);
+          return;
+        }
+        continue;
+      }
+      // insert before the current node
+      if (node === this.head || previousNode === undefined) {
+        // insert value as new head
+        this.#insertAsHead(value);
+        return;
+      }
+      while (previousNode.down !== undefined) {
+        descendedNodesByLayer.push(previousNode);
+        previousNode = previousNode.down;
+      }
+      const insertedNode = this.#insertAfter(value, previousNode);
+      this.#setupPromotions(insertedNode, descendedNodesByLayer);
+    }
+  }
+
+  #insertAfter(valueToInsert: T, node: SkipListNode<T>): SkipListNode<T> {
+    const lastNext = node.next;
+    node.next = new SkipListNode(valueToInsert);
+    node.next.next = lastNext;
+    return node.next;
+  }
+
+  #insertAsHead(value: T) {
+    let formerHeadNode = this.head!;
+    let newHeadNode = new SkipListNode(value);
+    this.head = newHeadNode;
+    while (true) {
+      // point to existing head column nodes until the original head values are re-inserted
+      newHeadNode.next = formerHeadNode;
+      if (formerHeadNode.down === undefined) {
+        break;
+      }
+      newHeadNode.down = new SkipListNode(value);
+      newHeadNode = newHeadNode.down;
+      formerHeadNode = formerHeadNode.down;
+    }
+    // remove and re-insert the original head value after the new head value
+    this.remove(formerHeadNode.data);
+    this.#insertAfter(formerHeadNode.data, newHeadNode);
+  }
+
+  #setupPromotions(
+    insertedNode: SkipListNode<T>,
+    descendedNodesByLayer: Array<SkipListNode<T>>,
+  ) {
+    let previousNode = insertedNode;
+    let numPromotions = 0;
+    for (let i = descendedNodesByLayer.length - 1; i >= 0; i--) {
+      if (
+        !this.#probabilityFunction() ||
+        numPromotions >= this.#maxPromotions
+      ) {
+        return;
+      }
+      // promote by inserting a node in the layer above the current layer
+      const leftNeighborNode = descendedNodesByLayer[i];
+      const newNode = new SkipListNode<T>(previousNode.data);
+      newNode.next = leftNeighborNode.next;
+      leftNeighborNode.next = newNode;
+      newNode.down = previousNode;
+      previousNode = newNode;
+      numPromotions += 1;
+    }
+    if (numPromotions < descendedNodesByLayer.length) {
+      return;
+    }
+
+    // perform any additional promotions by creating new layers
+    while (this.#probabilityFunction() && numPromotions < this.#maxPromotions) {
+      // promote both the head and the node in its new layer, promote the head
+      // first (the head node must be present in all layers)
+      const formerHead = this.head!;
+      this.head = new SkipListNode(formerHead.data);
+      this.head.down = formerHead;
+      // promote by inserting a node in the layer above the current layer
+      const newNode = new SkipListNode<T>(previousNode.data);
+      this.head.next = newNode;
+      newNode.down = previousNode;
+      previousNode = newNode;
+      numPromotions += 1;
+    }
   }
 
   remove(value: T) {
-    throw new Error("Not yet implemented.");
+    if (this.head === undefined) {
+      return;
+    }
+    let node: SkipListNode<T> | undefined;
+    if (this.head.data === value) {
+      // remove the head node column
+      node = this.head;
+      this.head = node.next;
+      while (node !== undefined) {
+        const nodeDown: SkipListNode<T> | undefined = node.down;
+        deleteObjectProperties(node);
+        node = nodeDown;
+      }
+      return;
+    }
+    // traverse the list
+    node = this.head.next;
+    let previousNode: SkipListNode<T> = this.head;
+    while (node !== undefined) {
+      if (node.data === value) {
+        previousNode.next = node.next;
+        deleteObjectProperties(node);
+        node = previousNode.down;
+        continue;
+      }
+      previousNode = node;
+      node = node.next;
+    }
+  }
+
+  get values(): ReadonlyArray<T> {
+    const array: Array<T> = [];
+    if (this.head === undefined) {
+      return array;
+    }
+    let lowestHeadNode: SkipListNode<T> | undefined = this.head;
+    while (lowestHeadNode.down !== undefined) {
+      lowestHeadNode = lowestHeadNode.down;
+    }
+    while (lowestHeadNode !== undefined) {
+      array.push(lowestHeadNode.data);
+      lowestHeadNode = lowestHeadNode.next;
+    }
+    return array;
   }
 }
